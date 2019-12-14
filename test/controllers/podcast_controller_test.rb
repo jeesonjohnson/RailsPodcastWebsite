872c15a17ff3c251048402fcc871cb76121e7e4a
Jeeson Johnson <jeesonjohnson@github.com>
2019-12-14 08:50:12 +0000

    Improved testing of user and friendship controllers

--- test/controllers/podcast_controller_test.rb
+++ test/controllers/podcast_controller_test.rb
@@ -2,6 +2,24 @@ require 'test_helper'
 
 class PodcastControllerTest < ActionDispatch::IntegrationTest
   def setup
+    @episode_data={:url=>"https://traffic.megaphone.fm/DGT7102510901.mp3", :title =>"Snacks Daily"}
   end
 
+  test 'Test palying of a podcast episode' do
+    # If a episode was sucesffully interperated, the appropraite response should contain the episode data, alongisde audio player url.
+
+    get '/playaudio', params: { audioUrl: @episode_data[:url], title: @episode_data[:title] }, xhr: true
+    assert_response :success
+    assert body.include? @episode_data[:title]
+    assert body.include? @episode_data[:url]
+  end
+
+
+  test 'Test searching for podcasts given a search term' do
+    # Search result after request to api's and such, should return a view with a list of podcasts, which incldues the name of the podcast
+    # This being as @episode_data.title is a valid existing podcast
+    get '/searchpodcasts', params: { podcast: @episode_data[:title] }, xhr: true
+    assert_response :success
+    assert body.include? @episode_data[:title]
+  end
 end
--- test/controllers/podcastapp_controller_test.rb
+++ test/controllers/podcastapp_controller_test.rb
@@ -2,6 +2,42 @@ require 'test_helper'
 
 class PodcastappControllerTest < ActionDispatch::IntegrationTest
   def setup
+    @user=users(:one)
+    sign_in users(:one)
+    @podcast=podcasts(:one)
   end
+
+  test 'Display the appropraite home page to the user' do
+    # Depending on weather differnt types of request are made, the user is presented with a differnt view.
+    get app_url
+    assert_response :success
+    assert_template layout: 'application'
+    assert_select 'title', I18n.t('app_title')
+    assert_select 'h1', I18n.t('app_title')
+    assert body.include? I18n.t('home.title')
+  end
+
+  test 'Display the appropraite home page to the user, as per AJAX request' do
+    # This response is provided if the request the user makes of a remote, AJAX test
+    get app_url, xhr: true
+    assert_response :success
+    assert body.include? I18n.t('home.title')
+  end
+
+
+  test 'Display the appropraite search page to the user' do
+    # This response is provided if the request the user makes of a remote, AJAX test
+    get app_search_url, xhr: true
+    assert_response :success
+    assert body.include? I18n.t('search.title')
+  end
+
+  test 'Display the appropraite podcast to a user, given an appropriate itunes_id request' do
+    # This response is provided if the request the user makes of a remote, AJAX test
+    get podcast_url,params: { itunes_id: @podcast.itunes_id}, xhr: true
+    assert_response :success
+    assert body.include? @podcast.name
+  end
+
 
 end

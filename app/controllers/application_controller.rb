bd54362e38050d15c476b58e5ac71d84e58de461
Jeeson Johnson <jeesonjohnson@github.com>
2019-12-16 01:27:33 +0000

    Improved the layout of login and registration page for the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,6 @@
 class ApplicationController < ActionController::Base
   #Prevent CSRF attacks by raising an exception.
   protect_from_forgery with: :exception
-  #Add ability to change locals if need be.
-  before_action :set_locale
 
-
-  private
-  #Function for changing locals to other languages if need be. 
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
-
-  def default_url_options(options = {})
-    {locale: I18n.locale}.merge options
-  end
 
 end
